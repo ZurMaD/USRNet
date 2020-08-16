25de8e64570a180dfa31c72e3cb86fd82c6fabe5
ZurMaD <pablo.diazv@pucp.edu.pe>
2020-08-16 00:18:29 -0500

    Update download_models.bash

--- download_models.bash
+++ download_models.bash
@@ -1,4 +1,4 @@
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qz8aaYOAMhoKn07VppFjRsDflYpxeVmz' -O /model_zoo/usrnet.pth
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1R5HKJzHJmou-3iUYd4cNLSyMeWXSOmD4' -O /model_zoo/usrgan.pth
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xPN26OW5YBN9-5QfK2BGnaovSutH7fLX' -O /model_zoo/usrnet_tiny.pth
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1U4BV42Qf0dtBOVbhUNRay6di3j_ioZVM' -O /model_zoo/usrgan_tiny.pth
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1qz8aaYOAMhoKn07VppFjRsDflYpxeVmz' -O ./model_zoo/usrnet.pth
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1R5HKJzHJmou-3iUYd4cNLSyMeWXSOmD4' -O ./model_zoo/usrgan.pth
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xPN26OW5YBN9-5QfK2BGnaovSutH7fLX' -O ./model_zoo/usrnet_tiny.pth
+wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1U4BV42Qf0dtBOVbhUNRay6di3j_ioZVM' -O ./model_zoo/usrgan_tiny.pth
